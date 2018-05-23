a463e72ccbb9e2377f3328d6a65a4f037f9e8c71
dinoilic <dino.ilic@gmail.com>
2018-05-23 22:56:03 +0200

    More code

--- algoritam.py
+++ algoritam.py
@@ -8,9 +8,25 @@ Created on Wed May 23 18:48:34 2018
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import csv
 
+scaler = MinMaxScaler()
 
 bldtyp = ['0-', '0+', 'A-', 'A+', 'B-', 'B+', 'AB-', 'AB+']
+
+bldtypdict = {
+    '0-': 0,
+    '0+': 1,
+    'A-': 2,
+    'A+': 3,
+    'B-': 4,
+    'B+': 5,
+    'AB-': 6,
+    'AB+': 7        
+}
+
 minsup = np.array([38, 115, 46, 100, 38, 23, 8, 16])
 optsup = np.array([58, 177, 71, 155, 60, 36, 13, 26])
 maxsup = np.array([78, 240, 96, 210, 82, 50, 18, 36])
@@ -32,6 +48,28 @@ def loss(x, i):
     else:
         return (200 / r) * (x - maxsup[i]) + 35
 
+def get_new_data(data, days_passed):
+    new_data = []
+    
+    data['freq_norm'] = data['frequency']
+    data['dist_norm'] = np.exp(data['distance'])
+    
+    data[['freq_norm', 'dist_norm']] = scaler.fit_transform(data[['frequency', 'distance']])
+    data['freq_norm'] = abs(1 - data['freq_norm'])
+    
+    for index, row in data.iterrows():
+        okay = 0
+        if row['sex'] == 'M' and (row['last_donation'] + days_passed) >= 90:
+            okay = 1
+        elif row['sex'] == 'Z' and (row['last_donation'] + days_passed) >= 120:
+            okay = 1
+            
+        if okay:
+            row['goodness'] = np.sqrt(row['dist_norm'] ** 2 +  row['freq_norm'] ** 2)
+            new_data.append(row)
+
+
+    return pd.DataFrame(new_data)
 
 def correct():
     pass
@@ -47,5 +85,27 @@ blddata = pd.DataFrame([
     cursup
 ], columns=bldtyp)
 
-# Week 0
 
+available_people = get_new_data(data, 0)
+available_people_sorted = available_people.sort_values(['goodness'], ascending=[True])
+
+plt.scatter(available_people_sorted[1:1000]['frequency'], available_people_sorted[1:1000]['distance'])
+
+cursup -= wekcon
+
+people_called = []
+
+for index, row in data.iterrows():
+    bldid = bldtypdict[row['blood_group']]
+    if cursup[bldid] < optsup[bldid]:
+        u = np.random.rand()
+        
+        if(u >= 0.8):    
+            cursup[bldid] += 1
+
+        people_called.append(row['id'])
+        
+with open('test.csv', 'w') as csvfile:
+    testwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    testwriter.writerow(people_called)
+    
